a2bf87662c5d0b1b572cc952a81fce55e1afae5b
DavidHernandezVilalta <david.hernandez.vilalta@est.fib.upc.edu>
2018-04-26 16:28:27 +0200

    UPDATE

--- PROPHidato/src/Partida.java
+++ PROPHidato/src/Partida.java
@@ -1,4 +1,18 @@
 
 public class Partida {
+	public Integer dificultat;
+	public Hidato hidato;
 	
+	public Partida createPartida(Integer diff) {
+		this.dificultat = diff;
+	}
+	
+	public boolean checkFinished(Hidato hidato) {
+		if (hidato.finished) return true;
+		return false;
+	}
+	
+	public void nextMove() {
+		
+	}
 }
--- PROPHidato/src/Ranking.java
+++ PROPHidato/src/Ranking.java
@@ -1,4 +1,33 @@
+import java.util.HashMap;
 
 public class Ranking {
+	HashMap<String, Integer> rank;
 	
+	public Ranking() {
+		rank = new HashMap<String, Integer>();
+	}
+	
+	public void llegirRanking() {
+		
+	}
+	
+	public void updateRanking() {
+		
+	}
+	
+	public int consultarPosicio(String nomUser) {
+		return;
+	}
+	
+	public Ranking globalRanking() {
+		return "el ranking global";
+	}
+	
+	public Ranking solvedRanking() {
+		return "el ranking solved";
+	}
+	
+	public Ranking createdRanking() {
+		return "el ranking created";
+	}
 }
