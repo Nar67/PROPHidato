9e7dc830c87ed658b53a0bbaeaf6bfec76a2e113
DavidHernandezVilalta <david.hernandez.vilalta@est.fib.upc.edu>
2018-04-29 17:34:39 +0200

    Controller a full

--- PROPHidato/src/ControlDomini.java
+++ PROPHidato/src/ControlDomini.java
@@ -1,68 +1,33 @@
 import java.util.Scanner;
 
+import com.sun.org.glassfish.gmbal.ManagedAttribute;
+
 public class ControlDomini {
 	
-	public Hidato inicialitzaManualHidato(String[][] matriu, String[] params) {
-		Board tauler;
-		switch(params[0]) {
-			case "H":
-				tauler = new HexagonBoard(params,  matriu);
-				break;
-			case "T":
-				tauler = new TriangleBoard(params,  matriu);
-				break;
-			default:
-				tauler = new SquareBoard(params, matriu);
-				break;
-		};
-		Hidato hidato = new Hidato(tauler);	
-		hidato.checkHidato();
-		return hidato;
-	}
-	
-	public Hidato inicialitzaAutoHidato(String[][] matriu, int diff, int randomi, int randomj) {
-		Board tauler;
-		switch(diff) {
-			case 1:
-				String params[] = {"Q", "C", Integer.toString(randomi), Integer.toString(randomj)};
-				tauler = new SquareBoard(params, matriu);
-				break;
-			case 2:
-				String params1[] = {"Q", "CA", Integer.toString(randomi), Integer.toString(randomj)};
-				tauler = new SquareBoard(params1, matriu);
-				break;
-			case 4:
-				String params3[] = {"H", "CA", Integer.toString(randomi), Integer.toString(randomj)};
-				tauler = new HexagonBoard(params3,  matriu);
-				break;
-			case 5:
-				String params4[] = {"T", "C", Integer.toString(randomi), Integer.toString(randomj)};
-				tauler = new TriangleBoard(params4,  matriu);
-				break;
-			case 6:	
-				String params5[] = {"T", "CA", Integer.toString(randomi), Integer.toString(randomj)};
-				tauler = new TriangleBoard(params5,  matriu);
-				break;
-			default:
-				String params2[] = {"H", "C", Integer.toString(randomi), Integer.toString(randomj)};
-				tauler = new HexagonBoard(params2,  matriu);
-				break;
+	public void llegeixHidato() {
+		Partida partida = new Partida();
+		Hidato hidato = partida.llegirTaulell();
+		while (!hidato.checkHidato()) {
+			System.out.println("L'hidato proposat no es resoluble");
+			hidato = partida.llegirTaulell();
 		}
-		Hidato hidato = new Hidato(tauler);
-		hidato.checkHidato();
-		return hidato;
 	}
 	
-	
-	public void jugaManualHidato(String[][] matriu, String[] params) {
-		Hidato hidato = inicialitzaManualHidato(matriu, params);
-		Partida partida = new Partida(hidato);		
-		partida.startPlaying();
+	public void generaHidato() {
+		Partida partida = new Partida();
+		System.out.println("Escoja dificultad: 1(muy fácil), 2(fácil), 3(normal), 4(difícil), 5(muy difícil), 6(experto).");
+		Scanner keyboard = new Scanner(System.in);
+		keyboard = new Scanner(System.in);
+		int diff = keyboard.nextInt();
+		Hidato hidato = partida.generarTaulell(diff);
+		System.out.println("Autogenerant hidato ... ");
+		Hidato hidatobuit = hidato;
+		while (!hidato.checkHidato()) {
+			System.out.println(" ... ");
+			hidato = partida.generarTaulell(diff);
+			hidatobuit = hidato;
+		}
+		hidatobuit.getTaulell().printBoard();
 	}
 	
-	public void jugaAutoHidato(String[][] matriu, int diff, int randomi, int randomj) {
-		Hidato hidato = inicialitzaAutoHidato(matriu, diff, randomi, randomj);
-		Partida partida = new Partida(hidato);
-		partida.startPlaying();
-	}
 }
--- PROPHidato/src/Main.java
+++ PROPHidato/src/Main.java
@@ -3,84 +3,13 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 	
-	private static void manualHidato() {
-		System.out.println("Introduzca un hidato válido\n");
-		Scanner keyboard = new Scanner(System.in);
-		keyboard = new Scanner(System.in);
-		String dades = keyboard.nextLine();
-		String params[] = dades.split(",");
-		String matriu[][] = new String[Integer.parseInt(params[2])][Integer.parseInt(params[3])];
-		for (int i = 0; i < matriu.length; ++i) {
-			String aux = keyboard.nextLine();
-			String aux2[] = aux.split(",");
-			for (int j = 0; j < aux2.length; ++j) {
-				matriu[i][j] = aux2[j]; 
-				System.out.print(matriu[i][j]);
-			}				
-			System.out.println();
-		}
-		ControlDomini controlador = new ControlDomini();
-		controlador.jugaManualHidato(matriu, params);
-	}
-	
-	private static void generarHidato() {
-		System.out.println("Escoja dificultad: 1(muy fácil), 2(fácil), 3(normal), 4(difícil), 5(muy difícil), 6(experto).");
-		Scanner keyboard = new Scanner(System.in);
-		keyboard = new Scanner(System.in);
-		int diff = keyboard.nextInt();
-		System.out.println();
-		System.out.println("Autogenerando Hidato ...");
-		
-		int randomi = ThreadLocalRandom.current().nextInt(3, 8);
-		int randomj = ThreadLocalRandom.current().nextInt(3, 8);
-		String matriu[][] = new String[randomi][randomj];
-		System.out.println(randomi);
-		System.out.println(randomj);
-		int randomstarti = ThreadLocalRandom.current().nextInt(0, randomi);
-		int randomstartj = ThreadLocalRandom.current().nextInt(0, randomj);
-		matriu[randomstarti][randomstartj] = "1";
-		
-		int hashtags = (randomi*randomj)/3;
-		for (int i = 0; i < hashtags; ++i) {
-			randomstarti = ThreadLocalRandom.current().nextInt(0, randomi);
-			randomstartj = ThreadLocalRandom.current().nextInt(0, randomj);
-			while (matriu[randomstarti][randomstartj] != null) {
-				randomstarti = ThreadLocalRandom.current().nextInt(0, randomi);
-				randomstartj = ThreadLocalRandom.current().nextInt(0, randomj);
-			}
-			matriu[randomstarti][randomstartj]= "#"; 
-		}
-		
-		randomstarti = ThreadLocalRandom.current().nextInt(0, randomi);
-		randomstartj = ThreadLocalRandom.current().nextInt(0, randomj);
-		while (matriu[randomstarti][randomstartj] != null) {
-			randomstarti = ThreadLocalRandom.current().nextInt(0, randomi);
-			randomstartj = ThreadLocalRandom.current().nextInt(0, randomj);
-		}
-		matriu[randomstarti][randomstartj] = Integer.toString((randomi*randomj)-hashtags);
-
-		for (int i = 0; i < matriu.length; ++i) {
-			for (int j = 0; j < matriu[0].length; ++j) {
-				if ((i != randomstarti || j != randomstartj) && matriu[i][j] == null) matriu[i][j] = "?";					
-			}
-		}
-		
-		for (int i = 0; i < matriu.length; ++i) {
-			for (int j = 0; j < matriu[0].length; ++j) {
-				System.out.print(matriu[i][j]);
-				System.out.print(",");
-			}
-			System.out.println();
-		}
-		ControlDomini controlador = new ControlDomini();
-		controlador.jugaAutoHidato(matriu, diff, randomi, randomj);
-	}
-	
 	public static void main(String[] args) {
 		System.out.println("Para autogenerar un Hidato pulse: 1; para generar su propio Hidato pulse: 2.\n");
 		Scanner keyboard = new Scanner(System.in);
 		int action = keyboard.nextInt();
-		if (action == 1) generarHidato();
-		else if (action == 2) manualHidato();
+		ControlDomini controlador = new ControlDomini();
+		if (action == 1) controlador.generaHidato();
+		else if (action == 2) controlador.llegeixHidato();
+		else System.out.print("No es una opció vàlida.");
 	}
 }
