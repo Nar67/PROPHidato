55cbcced035783d2fc0611c49cb9e614977cd044
Nars <narcis.terrado@gmail.com>
2018-05-02 13:08:25 +0200

    a

--- PROPHidato/src/Tests/AllTests.java
+++ PROPHidato/src/Tests/AllTests.java
@@ -1,5 +1,6 @@
 package Tests;
 
+import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -7,5 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({SquareBoardTest.class, TriangleBoardTest.class, HexagonBoardTest.class})
 public class AllTests {
-	public static void main(String[] args) throws Exception {}
+	public static void main(String[] args) throws Exception {
+		JUnitCore.main();
+	}
 }
