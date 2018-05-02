4db60c333ed13bbe7ec8c640c9c840c19a66d792
Roger Coll Aumatell <33873530+rogercoll@users.noreply.github.com>
2018-05-02 13:20:00 +0200

    Add files via upload

--- PROPHidato/src/Domini/Usuari.java
+++ PROPHidato/src/Domini/Usuari.java
@@ -1,24 +1,57 @@
 package Domini;
+import java.security.MessageDigest;
+import javax.xml.bind.DatatypeConverter;
 
 public class Usuari {
 		private String nom;
 		private String password;
 	
-		public Usuari() {
-			
-		}
-		
-		public void CreateUsuari(String nomu, String pass) {
+		public Usuari(String nomu, String pass) {
 			this.nom = nomu;
-			this.password = pass;
+			this.password = getSHA256Hash(pass);
 		}
-		
-		public Usuari GetUsuari() {
+		private String  bytesToHex(byte[] hash) {
+			        return DatatypeConverter.printHexBinary(hash);
+		}
+		private String getSHA256Hash(String data) {
+			        String result = null;
+			        try {
+			            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			            byte[] hash = digest.digest(data.getBytes("UTF-8"));
+			            return bytesToHex(hash); // make it printable
+			        }catch(Exception ex) {
+			            ex.printStackTrace();
+			        }
+			        return result;
+			    }
+
+		public Usuari GetUsuari(String nomu, String pass) {
+			//S'implementara en la seguent entrega ja que agafa un usuari de la base de dades
 			return this;
 		}
-		public void ChangePassword(String nomu, String pass) {
-			if(this.nom == nomu) {
-				this.password = pass;
-			}
+		public void printUsuari() {
+			System.out.println(this.nom);
+			System.out.println(this.password);
 		}
+		public void printNomUsuari() {
+			System.out.println(this.nom);
+		}
+		public void ChangePassword(String pass) {
+			String sha = getSHA256Hash(pass);
+			if(this.password.equals(sha)) {
+				System.out.println("Perdoni, ha introduit la mateixa contrasenya");
+			}
+			else this.password = sha;
+		}
+		public int MillorTemps(String dificultat) {
+			//retornarà el millor temps de l'usuari depenent de la dificultat
+			return 0;
+		}
+		public void SetRanking(String dificultat, Integer temps) {
+			//aqui introduirà els resultats d'un usuari en en ranking
+		}
+		public void EsborrarUsuari() {
+			//borrarà l'usuari de la base de Dades
+		}
+		
 }
