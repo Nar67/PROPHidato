0153dcade56e66361363aa6a3e6409c99ff1919d
Nar67 <narcis.tg@gmail.com>
2018-05-24 15:14:01 +0200

    f

--- PROPHidato/src/Persistencia/BoardStorage.java
+++ PROPHidato/src/Persistencia/BoardStorage.java
@@ -1,10 +1,24 @@
 package Persistencia;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import Domini.Board;
+import Domini.ControlDomini;
+import Domini.Partida;
+import Domini.SquareBoard;
 
 public class BoardStorage {
 	public static void main(String[] args) {
-		Gson gson = new Gson();
-		
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(SquareBoard.class, new InterfaceAdapter());
+		Gson gson = builder.create();
+		ControlDomini cDomini = new ControlDomini();
+		Partida partida = cDomini.generaHidato();
+		String p = gson.toJson(partida);
+		Partida part = gson.fromJson(p, Partida.class);
+		Board b = part.getHidato().getTaulell();
+		System.out.println("hitler");
+		System.out.println(b.getParams());
 	}
 }
